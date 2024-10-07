94da4850b252df80e91a3795adff98b7583b990f
gittjar <jarno.keranen@gmail.com>
2024-10-07 20:39:20 +0300

    carousel items to homepage

--- ts-frontend/src/components/HomePage.jsx
+++ ts-frontend/src/components/HomePage.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
 
 const HomePage = () => {
   const [showInfo, setShowInfo] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
   const handleClick = () => {
     setShowInfo(true);
@@ -10,21 +16,51 @@ const HomePage = () => {
 
   return (
     <div>
-      <div className="jumbotron">
-        <h1 className="display-4">Hello!</h1>
-        <p className="lead">Tämä on TaloSave. Verkkopohjainen tallennusohjelma sinun kiinteistöllesi. Kaikki oikeudet Jarno K. 2024.</p>
-        <hr className="my-4" />
-        <p>Testaa ohjelmaa ilmaiseksi!</p>
-        <hr className="my-4" />
-
-        <a className="btn btn-primary btn-lg" href="#" role="button" onClick={handleClick}>Katso lisätiedot!</a>
+            <div className="custom-indicators mt-2">
+        {[...Array(4)].map((_, i) => (
+          <div
+            key={i}
+            className={`custom-indicator ${i === index ? 'active' : ''}`}
+            onClick={() => setIndex(i)}
+          />
+        ))}
       </div>
+      
+      <Carousel activeIndex={index} pause={false} onSelect={handleSelect} className="custom-carousel">
+        <Carousel.Item>
+          <img
+            className="d-block w-100 img-fluid"
+            src="../assets/images/IMG_2727.WEBP"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 img-fluid"
+            src="../assets/images/IMG_2728.WEBP"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 img-fluid"
+            src="../assets/images/IMG_2729.WEBP"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 img-fluid"
+            src="../assets/images/IMG_2730.WEBP"
+            alt="Fourth slide"
+          />
+        </Carousel.Item>
+      </Carousel>
 
       {showInfo && (
         <div className="info-screen">
           <h3>Hello! Kokeile ohjelmaa siirtymällä <Link to="/login">login</Link> sivulle!</h3>
           <h4 className='text-warning'> käyttäjätunnus: smith <br></br>salasana: salasana</h4>
-
         </div>
       )}
     </div>
