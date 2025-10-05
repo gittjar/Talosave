8eaed9a507694c73faf92e6ab75cc1bbf3a6b968
Jarno K <jarno.keranen@gmail.com>
2025-10-05 19:45:43 +0300

    Updated navbar layout

--- ts-frontend/src/components/Navbar.jsx
+++ ts-frontend/src/components/Navbar.jsx
@@ -1,72 +1,154 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Container, Nav, Navbar, NavDropdown, Badge } from 'react-bootstrap';
+import { 
+  House, 
+  PersonPlus, 
+  BoxArrowInRight, 
+  BoxArrowRight, 
+  Buildings, 
+  GearFill, 
+  InfoCircle, 
+  ShieldCheck,
+  PersonCircle
+} from 'react-bootstrap-icons';
 import { jwtDecode } from 'jwt-decode';
 
 const NavBar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
   const location = useLocation();
 
   useEffect(() => {
     const token = localStorage.getItem('userToken');
+    const storedUsername = localStorage.getItem('username');
+    
     if (token) {
       const decodedToken = jwtDecode(token);
       const currentTime = Date.now() / 1000;
       if (decodedToken.exp < currentTime) {
         localStorage.removeItem('userToken');
+        localStorage.removeItem('username');
         setIsLoggedIn(false);
+        setUsername('');
       } else {
         setIsLoggedIn(true);
+        setUsername(storedUsername || '');
       }
     } else {
       setIsLoggedIn(false);
+      setUsername('');
     }
-  }, [location]); // Re-render when the location changes
+  }, [location]);
 
   const handleLogout = () => {
     localStorage.removeItem('userToken');
+    localStorage.removeItem('username');
     setIsLoggedIn(false);
+    setUsername('');
   };
 
   return (
-    <nav className='navibar'>
-    <Navbar expand="lg" className="bg-body-tertiary navibar-color">
+    <Navbar expand="lg" className="modern-navbar shadow-sm" sticky="top">
       <Container>
-        <Navbar.Brand as={Link} to="/home">TaloSave App Beta (v.1.0)</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        {/* Brand */}
+        <Navbar.Brand as={Link} to="/home" className="brand-link">
+          <div className="d-flex align-items-center">
+            <div className="brand-icon me-2">
+              <Buildings size={24} />
+            </div>
+            <div>
+              <span className="brand-name">TaloSave</span>
+              <Badge bg="secondary" className="ms-2 brand-badge">Beta</Badge>
+            </div>
+          </div>
+        </Navbar.Brand>
+
+        {/* Mobile Toggle */}
+        <Navbar.Toggle aria-controls="navbar-nav" className="border-0" />
+        
+        <Navbar.Collapse id="navbar-nav">
+          {/* Main Navigation */}
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-            <Nav.Link as={Link} to="/create-user">Create User</Nav.Link>
+            <Nav.Link as={Link} to="/home" className="nav-item-modern">
+              <House size={16} className="me-2" />
+              Etusivu
+            </Nav.Link>
+            
+            {!isLoggedIn && (
+              <Nav.Link as={Link} to="/create-user" className="nav-item-modern">
+                <PersonPlus size={16} className="me-2" />
+                Rekisteröidy
+              </Nav.Link>
+            )}
+          </Nav>
+
+          {/* User Navigation */}
+          <Nav className="ms-auto">
             {isLoggedIn ? (
               <>
-                <Nav.Link as={Link} to="/login" onClick={handleLogout}>Logout</Nav.Link>
-                <Nav.Link as={Link} to="/mypage">Omat rakennukset</Nav.Link>
-                <Nav.Link as={Link} to="/usersettings">Asetukset</Nav.Link>
+                <Nav.Link as={Link} to="/mypage" className="nav-item-modern">
+                  <Buildings size={16} className="me-2" />
+                  Rakennukset
+                </Nav.Link>
+                
+                {/* User Dropdown */}
+                <NavDropdown 
+                  title={
+                    <span className="user-dropdown-title">
+                      <PersonCircle size={18} className="me-2" />
+                      {username || 'Käyttäjä'}
+                    </span>
+                  }
+                  id="user-dropdown" 
+                  className="user-dropdown"
+                  align="end"
+                >
+                  <NavDropdown.Item as={Link} to="/usersettings" className="dropdown-item-modern">
+                    <GearFill size={14} className="me-2" />
+                    Tilin asetukset
+                  </NavDropdown.Item>
+                  
+                  <NavDropdown.Divider />
+                  
+                  <NavDropdown.Item as={Link} to="/login" onClick={handleLogout} className="dropdown-item-modern logout-item">
+                    <BoxArrowRight size={14} className="me-2" />
+                    Kirjaudu ulos
+                  </NavDropdown.Item>
+                </NavDropdown>
               </>
             ) : (
-              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/login" className="nav-item-modern login-btn">
+                <BoxArrowInRight size={16} className="me-2" />
+                Kirjaudu sisään
+              </Nav.Link>
             )}
-            <NavDropdown title="Käyttöehdot" id="basic-nav-dropdown">
-              
-              <NavDropdown.Item href="#action/3.1" disabled hidden>Link 1</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/data-protection">Tietosuoja</NavDropdown.Item>
-              <NavDropdown.Divider />
 
-              <NavDropdown.Item as={Link} to="/program-info">Ohjelman tiedot</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4" hidden>Copyright JarnoK 2024</NavDropdown.Item>
+            {/* Info Dropdown */}
+            <NavDropdown 
+              title={
+                <span>
+                  <InfoCircle size={16} />
+                </span>
+              }
+              id="info-dropdown" 
+              className="info-dropdown"
+              align="end"
+            >
+              <NavDropdown.Item as={Link} to="/data-protection" className="dropdown-item-modern">
+                <ShieldCheck size={14} className="me-2" />
+                Tietosuoja
+              </NavDropdown.Item>
+              
+              <NavDropdown.Item as={Link} to="/program-info" className="dropdown-item-modern">
+                <InfoCircle size={14} className="me-2" />
+                Ohjelman tiedot
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-
       </Container>
-
     </Navbar>
-    </nav>
-
   );
 };
 
